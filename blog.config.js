7e3d7baf472d22331270c3ee670aedb01dd71c3b
lee199111 <lee199111@gmail.com>
2022-11-24 13:57:04 +0800

    change tangly to lee199111

--- blog.config.js
+++ blog.config.js
@@ -52,11 +52,11 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com', // 邮箱
+  CONTACT_EMAIL: 'lee199111@gmail.com', // 邮箱
   CONTACT_WEIBO: '', // 你的微博个人主页
   CONTACT_TWITTER: '', // 你的twitter个人主页
-  CONTACT_GITHUB: 'https://github.com/tangly1024', // 你的github个人主页
-  CONTACT_TELEGRAM: 'https://t.me/tangly_1024', // 你的telegram 地址 例如 https://t.me/tangly_1024
+//   CONTACT_GITHUB: 'https://github.com/lee199111', // 你的github个人主页
+  CONTACT_TELEGRAM: 'https://t.me/lee199111', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
 
